d5d13c0d6a0a1f602d0655ec9c3189faf96b5e1e
giuliaballestrero <giulia.ballestrero@gmail.com>
2022-11-30 15:39:08 +0100

    Add numbers in grid

--- js/script.js
+++ js/script.js
@@ -29,6 +29,8 @@ const myGrid = document.getElementById('myGrid');
     const newSquare = document.createElement('div');
     //assegno la mia classe css al nuovo div
     newSquare.classList.add('square')
+    //creo un p dove inserire i numeri da 1 a 100
+    newSquare.innerHTML = '<span class="p-1">' + (i + 1) + '</span>'
     //aggiungo il nuovo div alla griglia
     myGrid.appendChild(newSquare);
   }
