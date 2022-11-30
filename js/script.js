df8e356cf2561562edfefa53f72a3880358f1b78
giuliaballestrero <giulia.ballestrero@gmail.com>
2022-11-30 15:04:47 +0100

    Create grid in btn addeventlistener

--- js/script.js
+++ js/script.js
@@ -4,7 +4,10 @@
 L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+Quando l'utente clicca su ogni cella, la cella cliccata 
+si colora di azzurro ed emetto un messaggio in console con 
+il numero della cella cliccata.
+
 Ad esempio:
 Di cosa ho bisogno per generare i numeri?
 Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
@@ -12,3 +15,23 @@ Le validazioni e i controlli possiamo farli anche in un secondo momento.
  *
  */
 
+
+//creo un evento che si scatena cliccando sul bottone "play"
+document.getElementById('myBtn').addEventListener('click', function() {
+
+//recupero il div griglia nel documento
+
+const myGrid = document.getElementById('myGrid');
+
+//creo un ciclo per aggiungere i 100 quadrati alla griglia
+  for (let i = 0; i < 100; i++) {
+    //creo il div
+    const newSquare = document.createElement('div');
+    //assegno la mia classe css al nuovo div
+    newSquare.classList.add('square')
+    //aggiungo il nuovo div alla griglia
+    myGrid.appendChild(newSquare);
+  }
+
+
+});
